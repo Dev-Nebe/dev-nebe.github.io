1807adc4d3b4f8b609e9006159c0551cff3fc9ae
Nebe <cp.nebe@gmail.com>
2020-01-13 14:09:03 +0100

    Set up service worker.

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const cacheName = 'cache-v1';
+
+let precacheResources = [
+  '/',
+  '/asets/css/main.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service worker is being installed');
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(precacheResources))
+    .catch(err => console.log(err));
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker is being activated');
+});
+
+self.addEventListener('fetch', (event)=> {
+  console.log(`Fetch request has been intercepted for ${event.request.url}`);
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      } else {
+        return fetch(event.request);
+      }
+    })
+      .catch(err => console.log(err));
+  );
+});
